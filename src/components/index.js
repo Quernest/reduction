01f38c3aa3b0bce523579b520b8d0dd0cfa63c61
quernest <a.zhit4enko@gmail.com>
2019-01-01 20:07:05 +0300

    home page & header

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-export { default as HomePage } from './HomePage';
+export { default as HomePage } from './Home';
 export { default as Header } from './Header';
 export {
   default as PrincipalComponentAnalysis,
