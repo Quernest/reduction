244a077b7f374bb753dc88c84e803b8a094e5fcb
john anthony <a.zhit4enko@gmail.com>
2019-06-02 21:05:29 +0300

    removed translation backend

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,12 +1,14 @@
 import i18n from 'i18next';
-import Backend from 'i18next-xhr-backend';
+// import Backend from 'i18next-xhr-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
+import translationEN from './locales/en/translation.json';
+import translationUK from './locales/uk/translation.json';
 
 i18n
   // load translation using xhr -> see /public/locales
   // learn more: https://github.com/i18next/i18next-xhr-backend
-  .use(Backend)
+  // .use(Backend)
   // detect user language
   // learn more: https://github.com/i18next/i18next-browser-languageDetector
   .use(LanguageDetector)
@@ -15,12 +17,19 @@ i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
+    resources: {
+      en: {
+        translation: translationEN,
+      },
+      uk: {
+        translation: translationUK
+      }
+    },
     fallbackLng: 'en',
-    debug: true,
+    debug: false,
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
-    ns: ['translation']
   });
 
 export default i18n;
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { ThemeProvider } from "@material-ui/styles";
-import React, { Suspense } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import { AppRouter } from "./router";
 import { theme } from "./theme";
@@ -10,10 +10,8 @@ import './i18n';
 // i18n translations might still be loaded by the xhr backend
 // use react's Suspense
 ReactDOM.render(
-  <Suspense fallback="loading">
-    <ThemeProvider theme={theme}>
-      <AppRouter />
-    </ThemeProvider>
-  </Suspense>,
+  <ThemeProvider theme={theme}>
+    <AppRouter />
+  </ThemeProvider>,
   document.getElementById("root")
 );
